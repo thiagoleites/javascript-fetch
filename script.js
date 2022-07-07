6a4053138555ac54a88052149d530ea3d93faa4e
Thiago Leite <thiagoleites@outlook.com.br>
2022-07-07 20:04:44 -0300

    Arquivo inicial HTML

---
+++ script.js
@@ -0,0 +1,19 @@
+const button = document.getElementById("getDados")
+const lista = document.getElementById("lista")
+
+function fetchApiData() {
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then( response => response.json() )
+    .then (data => {
+      
+      data.map((item) => {
+        const li = document.createElement("li")
+
+        li.setAttribute('class', 'list-group-item')
+        li.innerHTML = item.title
+        lista.appendChild(li)
+      })
+    })
+}
+
+button.addEventListener("click", fetchApiData)
